61790dccc67790281b013ec8d0b446829f5a4b32
mathieu <mathieu@disko.fr>
2013-09-30 17:03:17 +0200

    [feature/mathieu] Create a login form and send data to node

--- node/server.js
+++ node/server.js
@@ -1,2 +1,18 @@
-var io = require('socket.io');
-io.listen(7777);
+// Init socket IO, listen on port 7777
+var io = require('socket.io').listen(7777);
+
+/**
+ * On connection, when a user is connected
+ *
+ * @author Mathieu MARCHOIS <mathieu.marchois@gmail.com>
+ */
+io.sockets.on('connection', function(socket) {
+
+    /**
+     * When a user join the chat room
+     */
+    socket.on('joinChat', function(data) {
+        console.log(data);
+    });
+});
+
--- web/js/controller/ChatController.js
+++ web/js/controller/ChatController.js
@@ -1,3 +1,46 @@
-app.controller('ChatController', function($scope, socket) {
-    $scope.messages = [];
+/**
+ * Chat controller
+ *
+ * @author Mathieu MARCHOIS <mathieu.marchois@gmail.com>
+ */
+app.controller('ChatController', function($scope, socket)
+{
+    /**
+     * Store chat messages
+     * @type {Array}
+     */
+    $scope.messages = [
+        {
+            user    : 'Administrator',
+            message : 'Welcome on the chat',
+            date    : new Date().toLocaleString()
+        }
+    ];
+
+    /**
+     * User's pseudo
+     * @type string
+     */
+    $scope.pseudo = '';
+
+    /**
+     * Display error
+     * @type {string}
+     */
+    $scope.error = '';
+
+    /**
+     * Create a user on login form
+     *
+     * @param string field
+     */
+    $scope.createUser = function (field)
+    {
+        if (field) {
+              $scope.pseudo = field;
+              socket.emit('joinChat', $scope.pseudo);
+        } else {
+            $scope.error = 'You must choose your pseudo.';
+        }
+    }
 });
